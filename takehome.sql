c7088aa2ed4f3170b79f2a74edf89897e3ea44bd
Marcella Maki <marcellamaki@gmail.com>
2017-12-31 16:07:10 -0500

    thorugh 10

--- takehome.sql
+++ takehome.sql
@@ -13,3 +13,20 @@ SELECT district FROM city GROUP BY district ORDER BY SUM(population) DESC LIMIT 
 5. 
 SELECT indepyear, COUNT(*) FROM country WHERE indepyear IS NOT NULL GROUP BY indepyear ORDER BY COUNT(*) DESC LIMIT 1
 
+6. 
+SELECT name FROM country ORDER BY population DESC LIMIT 1
+
+7.
+SELECT name, population FROM city WHERE countrycode = (SELECT countrycode FROM city GROUP BY countrycode ORDER BY SUM(population) DESC LIMIT 1) ORDER BY population DESC LIMIT 1
+
+8. 
+SELECT MAX(population), countrycode, name FROM city GROUP BY countrycode, name ORDER BY MAX(population) DESC
+
+9. 
+SELECT district FROM countrylanguage
+LEFT JOIN city on countrylanguage.countrycode = city.countrycode WHERE language = 'English' AND IsOfficial ='T'  GROUP BY district ORDER BY SUM(population) DESC LIMIT 1
+
+10. 
+SELECT name FROM city LEFT JOIN countrylanguage on city.countrycode = countrylanguage.countrycode WHERE language = 'Spanish' AND IsOfficial ='T' ORDER BY population ASC LIMIT 1
+
+
