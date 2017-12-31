1d9f82ea4f6f59155c271c7bf2e6f4a8a97d4ef2
Marcella Maki <marcellamaki@gmail.com>
2017-12-31 16:06:22 -0500

    completed through 5

--- takehome.sql
+++ takehome.sql
@@ -1,1 +1,15 @@
-takehome.sql 
+1. 
+SELECT name, population FROM city ORDER BY population DESC LIMIT 1
+
+2. 
+SELECT SUM(population) FROM city
+
+3. 
+SELECT district FROM city GROUP BY district ORDER BY COUNT(district) DESC LIMIT 1
+
+4. 
+SELECT district FROM city GROUP BY district ORDER BY SUM(population) DESC LIMIT 1;
+
+5. 
+SELECT indepyear, COUNT(*) FROM country WHERE indepyear IS NOT NULL GROUP BY indepyear ORDER BY COUNT(*) DESC LIMIT 1
+
